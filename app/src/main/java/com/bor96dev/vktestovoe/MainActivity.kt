3a475c8413a060163d9b015a9e3fbf56c5c973b9
jakobakoba <jakobakoba@gmail.com>
2023-05-07 16:25:15 +0600

    Initial commit

---
+++ app/src/main/java/com/bor96dev/vktestovoe/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.bor96dev.vktestovoe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bor96dev.vktestovoe.ui.theme.VkTestovoeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VkTestovoeTheme {
+                // A surface container using the 'background' color from the theme
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = {Text(text = "Files")})
+                    },
+                    content = {
+
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    VkTestovoeTheme {
+        Greeting("Android")
+    }
+}
